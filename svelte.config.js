31a7cbae39279aff96ad573e442a8eaba7ec65ad
Your Name <you@example.com>
2024-07-02 18:16:27 +0200

    vitePreprocess

--- svelte.config.js
+++ svelte.config.js
@@ -11,17 +11,29 @@
 // };
 //
 // export default config;
-import adapter from "@sveltejs/adapter-static";
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-export default {
-  kit: {
-    adapter: adapter({
-      fallback: "404.html",
-    }),
-  },
-  paths: {
-    base: '/carbon-violence'
-  }
+const dev = process.argv.includes('dev');
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+    preprocess: vitePreprocess(),
+
+    kit: {
+        adapter: adapter({
+            pages: 'build',
+            assets: 'build',
+            fallback: undefined,
+            precompress: false,
+            strict: true
+        }),
+        paths: {
+            base: dev ? '' : process.env.BASE_PATH,
+        }
+    }
 };
+
+export default config;
 
 
