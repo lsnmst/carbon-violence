acb2c41d181bbf9a60147590c634756af7789c6d
Your Name <you@example.com>
2024-07-02 15:52:47 +0200

    Updates

---
+++ _app/immutable/nodes/3.9mi1BTbO.js
@@ -0,0 +1,23 @@
+import{s as i,n as a}from"../chunks/scheduler.Wcy2_qxX.js";import{S as n,i as h,e as c,c as d,k as f,g as l,d as m}from"../chunks/index.sVORdB3n.js";function p(o){let e,r=`<h2>About</h2> <p>The Offset Archive is a searchable database of every project listed in the
+    four major carbon registories: <a href="https://acrcarbon.org/">American Carbon Registry (ACR)</a>,
+    <a href="https://www.climateactionreserve.org/">Climate Action Reserve (CAR)</a>, <a href="https://www.goldstandard.org/">Gold Standard</a>, and
+    <a href="https://verra.org/">Verra</a>. These platforms make up the majority
+    of the voluntary carbon market and record lists of various carbon offsetting
+    projects, as well as the different methodologies that underlie them. The
+    voluntary market is one of two distinct domains for carbon offsetting today
+    and is open to the public to develop or purchase carbon offsetting products.
+    The other domain is the mandatory or “compliance” marketplaces that form
+    part of regulatory schemes run by governments and which are the basis for
+    reaching international climate commitments. Despite high hopes at the most
+    recent COP meeting in 2023 for regulation of voluntary carbon market, an
+    agreement on key rules for carbon trading was not reached. Voluntary markets
+    remain unregulated by the UN or governmental bodies.</p> <p>This archive is divided into two sections. The first lists all offset
+    projects which are the various intiatives listed on registries to reduce or
+    remove carbon. Each project has been approved by the registry where it is
+    listed. The second dataset consists of offset sales data. This lists every
+    entity that has bought carbon offsets from a registry.</p> <p>This database has been scraped and designed by Sam Lavigne. Sales or
+    &quot;retirement&quot; data is from <a href="https://gspp.berkeley.edu/research-and-impact/centers/cepp/projects/berkeley-carbon-trading-project">The Berkeley Carbon Trading Project</a>. Code for this project can be found on
+    <a href="https://github.com/antiboredom/offset-archive">GitHub</a>. The
+    Offset Archive is part of a collaborative artistic research project
+    <em>Offset</em>, by <a href="https://lav.io">Sam Lavigne</a> and
+    <a href="https://tegabrain.com">Tega Brain</a>.</p>`;return{c(){e=c("article"),e.innerHTML=r},l(t){e=d(t,"ARTICLE",{"data-svelte-h":!0}),f(e)!=="svelte-6oj9ef"&&(e.innerHTML=r)},m(t,s){l(t,e,s)},p:a,i:a,o:a,d(t){t&&m(e)}}}class u extends n{constructor(e){super(),h(this,e,null,p,i,{})}}export{u as component};
---
+++ _app/immutable/nodes/4.lDMN2Wq0.js
@@ -0,0 +1,1 @@
+import{s}from"../chunks/scheduler.Wcy2_qxX.js";import{S as t,i as e}from"../chunks/index.sVORdB3n.js";class l extends t{constructor(o){super(),e(this,o,null,null,s,{})}}export{l as component};
