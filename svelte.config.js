fddebfc1f8ea40fd36790aaca794da55fccb9fdb
Your Name <you@example.com>
2024-07-02 17:45:22 +0200

    process.env.PUBLIC_BASE_PATH

--- svelte.config.js
+++ svelte.config.js
@@ -11,29 +11,19 @@
 // };
 //
 // export default config;
-import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import adapter from "@sveltejs/adapter-static";
 
 const dev = process.argv.includes('dev');
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-    preprocess: vitePreprocess(),
-
-    kit: {
-        adapter: adapter({
-            pages: 'build',
-            assets: 'build',
-            fallback: undefined,
-            precompress: false,
-            strict: true
-        }),
-        paths: {
-            base: dev ? '' : process.env.BASE_PATH,
-        }
-    }
+export default {
+  kit: {
+    adapter: adapter({
+      fallback: "200.html",
+    }),
+  },
+  paths: {
+    base: process.env.PUBLIC_BASE_PATH,
+  },
 };
-
-export default config;
 
 
