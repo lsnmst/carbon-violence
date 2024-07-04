21fd28e4ba5f916df5feb64ad5b1c387c27d5ea5
Your Name <you@example.com>
2024-07-04 11:05:34 +0200

    Updates

---
+++ _app/immutable/chunks/paths.dMufzS1M.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_194dcu4?.base??"/carbon-violence",e=globalThis.__sveltekit_194dcu4?.assets??s;export{e as a,s as b};
---
+++ _app/immutable/entry/start.U3tLDaMy.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.G5HaJFFb.js";export{t as start};
